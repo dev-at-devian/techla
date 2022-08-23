30228b01d1d5fe6a5b9cb6adf639cba06abaa350
alic3woodstock <alic3woodstock@gmail.com>
2022-08-23 15:18:01 -0300

    Arquivos de funções com a função getNota

---
+++ src/components/Funcoes.js
@@ -0,0 +1,17 @@
+export const getNota = (str) => {
+    if (str == "C4-4n") {
+        return [3, 60, 2.5]
+    };
+
+    if (str == "D4-4n") {
+        return [3, 62, 2.5]
+    };
+
+    if (str == "E4-4n") {
+        return [3, 64, 2.5]
+    };
+
+    if (str == "F4-4n") {
+        return [3, 65, 2.5]
+    };
+}
